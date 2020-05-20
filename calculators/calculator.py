6c200feda5c52659b08c514ce9dee0c7117935fa
ssrokyz <ssrokyz@gmail.com>
2020-05-20 16:52:45 +0900

    for ss_model

--- calculators/calculator.py
+++ calculators/calculator.py
@@ -42,8 +42,8 @@ def compare_atoms(atoms1, atoms2, tol=1e-15):
 
 all_properties = ['energy', 'forces', 'stress', 'dipole',
             'charges', 'magmom', 'magmoms', 'free_energy', 'atomic_energies',
-            'e_alpha_z', 'e_ewald', 'e_hartree', 'e_exchange', 'e_vxc_exc', 'e_vdw',
-            'e_paw_double1', 'e_paw_double2', 'e_t_s', 'e_eigen', 'e_atomic', 'e_solv'] ## ssrokyz
+            'e_coulomb', 'e_alpha_z', 'e_ewald', 'e_hartree', 'e_exchange', 'e_vxc_exc', 'e_vdw',
+            'e_paw_double', 'e_paw_double1', 'e_paw_double2', 'e_t_s', 'e_eigen', 'e_atomic', 'e_solv'] ## ssrokyz
 
 
 all_changes = ['positions', 'numbers', 'cell', 'pbc',
--- calculators/singlepoint.py
+++ calculators/singlepoint.py
@@ -23,8 +23,8 @@ class SinglePointCalculator(Calculator):
             if value is None:
                 continue
             if property in ['energy', 'magmom', 'free_energy',
-                'e_alpha_z', 'e_ewald', 'e_hartree', 'e_exchange', 'e_vxc_exc', 'e_vdw',
-                'e_paw_double1', 'e_paw_double2', 'e_t_s', 'e_eigen', 'e_atomic', 'e_solv']:
+                'e_coulomb', 'e_alpha_z', 'e_ewald', 'e_hartree', 'e_exchange', 'e_vxc_exc', 'e_vdw',
+                'e_paw_double', 'e_paw_double1', 'e_paw_double2', 'e_t_s', 'e_eigen', 'e_atomic', 'e_solv']:
                 self.results[property] = value
             else:
                 self.results[property] = np.array(value, float)
